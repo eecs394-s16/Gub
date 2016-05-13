35c55c720fec8b7808d98a171cfae766688430f3
Athif Wulandana <athif@u.northwestern.edu>
2016-05-12 20:42:37 -0500

    Push notifications working on computer

--- www/js/app.js
+++ www/js/app.js
@@ -3,10 +3,17 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'firebase', 'ngTagsInput'])
+angular.module('starter', ['ionic','ionic.service.core', 'firebase', 'ngTagsInput'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
+    var push = new Ionic.Push({
+      "debug": true
+    });
+    push.register(function(token) {
+      console.log("Device token:",token.token);
+      push.saveToken(token);
+    });
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
